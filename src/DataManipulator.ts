a8ce9766675bab3bb13a8ca46d890d7d34ee0c05
Abhishek Kumar <21je0028@iitism.ac.in>
2023-12-26 16:04:49 +0530

    fixed the broken tsx file

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,32 @@
 import { ServerRespond } from './DataStreamer';
 
-export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+interface Row {
+  ratio: number;
+  upper_bound: number;
+  lower_bound: number;
+  trigger_alert: number | undefined;
+  price_abc: number;
+  price_def: number;
+  timestamp: Date;
 }
 
-
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    const price_abc = /* calculate price_abc based on serverRespond */;
+    const price_def = /* calculate price_def based on serverRespond */;
+    const ratio = price_abc / price_def;
+    const upper_bound = ratio * 1.1; // Adjust as needed
+    const lower_bound = ratio * 0.99; // Adjust as needed
+    const trigger_alert = ratio > upper_bound || ratio < lower_bound ? ratio : undefined;
+
+    return {
+      ratio,
+      upper_bound,
+      lower_bound,
+      trigger_alert,
+      price_abc,
+      price_def,
+      timestamp: new Date(),
+    };
   }
 }
